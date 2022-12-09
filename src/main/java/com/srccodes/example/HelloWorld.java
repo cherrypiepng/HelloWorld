f4b9c7f930e04507350192bf63c805316dc12b36
cherrypiepng <alina7433111@gmail.com>
2022-12-09 16:44:20 +0200

    secondcommit

---
+++ src/main/java/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,43 @@
+package com.srccodes.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloWorld() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 response.setContentType("text/html");
+	     PrintWriter printWriter = response.getWriter();
+	     printWriter.println("<h1>Hello World!</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+	    PrintWriter printWriter = response.getWriter();
+	    printWriter.println("<h1>Hello World!</h1>");
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+	    PrintWriter printWriter = response.getWriter();
+	    printWriter.println("<h1>Hello World!</h1>");
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+	    PrintWriter printWriter = response.getWriter();
+	    printWriter.println("<h1>Hello World!</h1>");
+	}
+
+}
